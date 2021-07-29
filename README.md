f95c3161223aaa85b2973e3da0d2dfa59d7072e0
Ahmed Elazazy <ahmed.elazazy@hotmail.com>
2021-07-29 12:52:31 +0200

    Adding saving results to text file

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 ### Usage
+
 - Update `.env`
 - `npm run start`
 - The results will show in console
+- The results will be saved in `output.txt`
 
+### Sample Command
 
+```
+ROUNDS=20 EXP="Experiment 1" npm run start
+```
+
+### Environment Variables
+
+| Variable          | Description                                                |
+| ----------------- | ---------------------------------------------------------- |
+| PAGESPEED_API_KEY | Google Page Speed Insights API Key                         |
+| URL               | The page URL to measure its performance                    |
+| DEVICE            | Run the performance test on mobile or desktop              |
+| ROUNDS            | Number of rounds to run the test                           |
+| EXP               | The name of the experiment to be saved in the `output.txt` |
--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+const fs = require("fs");
 const axios = require("axios");
+
 require("dotenv").config();
 
-const PAGESPEED_API_KEY = process.env.PAGESPEED_API_KEY;
-const URL = process.env.URL;
-const DEVICE = process.env.DEVICE;
+const { PAGESPEED_API_KEY, URL, DEVICE, EXP } = process.env;
 const ROUNDS = +process.env.ROUNDS;
 
 const getScoresSeq = async () => {
 	try {
+		let scores = [];
 		for (let i = 1; i <= ROUNDS; i++) {
 			const result = await calculatePerformance(URL, DEVICE);
-			console.log(i, result);
+			console.log(`#${i}:`, result);
+			scores.push(result);
 		}
+		return scores;
 	} catch (err) {
 		console.error(err);
 	}
@@ -19,15 +22,24 @@ const getScoresSeq = async () => {
 
 const calculatePerformance = async (url, device) => {
 	try {
-		const lhr = await callAPI(url, device);
+		const response = await axios.get("https://www.googleapis.com/pagespeedonline/v5/runPagespeed", {
+			params: {
+				key: PAGESPEED_API_KEY,
+				url: url,
+				strategy: device,
+			},
+		});
 
-		if (!lhr) {
+		if (!response || !response.data || !response.data.lighthouseResult) {
 			console.error("Error while calling lighthouse");
 			return;
 		}
 
-		const summary = {
+		const lhr = response.data.lighthouseResult;
+
+		return {
 			Date: today(),
+			Time: now(),
 			Page: lhr.finalUrl,
 			Device: device,
 			"Performance Score": Math.trunc(lhr.categories.performance.score * 100),
@@ -38,35 +50,25 @@ const calculatePerformance = async (url, device) => {
 			"Total Blocking Time (ms)": lhr.audits["total-blocking-time"].displayValue,
 			"Cumulative Layout Shift": lhr.audits["cumulative-layout-shift"].displayValue,
 		};
-
-		return summary;
 	} catch (err) {
 		console.error(err);
 	}
 };
 
-const callAPI = async (page, device) => {
-	try {
-		const json = await axios.get("https://www.googleapis.com/pagespeedonline/v5/runPagespeed", {
-			params: {
-				key: PAGESPEED_API_KEY,
-				url: page,
-				strategy: device,
-			},
-		});
-
-		return json.data.lighthouseResult;
-	} catch (err) {
-		console.error(err);
-	}
+const saveResultToFile = (data) => {
+	const formatted = data.map((r, i) => ({ [`#${i + 1}`]: r }));
+	fs.appendFileSync("output.txt", `\r\n\r\n############## ${now()} - ${EXP || ""} ##############\r\n\r\n`);
+	fs.appendFileSync("output.txt", JSON.stringify(formatted, null, 2));
 };
 
-function today(separator) {
-	let todayDate = new Date().toLocaleString("en-US", { timeZone: "America/New_York" }).split(",")[0];
-	if (separator) return todayDate.replace("/", separator);
-	return todayDate;
-}
+const today = () => new Date().toLocaleString("en-US", { timeZone: "America/New_York" }).split(",")[0];
+const now = () => new Date().toLocaleString("en-US", { timeZone: "America/New_York" });
 
 (async function () {
-	await getScoresSeq();
+	const scores = await getScoresSeq();
+
+	if (scores) {
+		saveResultToFile(scores);
+		console.log("Updated output.txt");
+	}
 })();
